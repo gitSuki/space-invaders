5921ab4e4f8b3d86e6dca7fbc3cba5d638f1fd03
gitSuki <collinbrown226@gmail.com>
2023-02-05 16:29:47 -0600

    Draw enemies on the board

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
@@ -14,16 +12,37 @@ type Bullet struct {
 	color     rl.Color
 }
 
+type Enemy struct {
+	posX int32
+	posY int32
+	img  rl.Texture2D
+}
+
 func main() {
 	screenSize := int32(600)
 	playerCoordinates := [2]int32{25, 525}
 	bullets := []Bullet{}
+	enemies := []Enemy{}
 	shouldShoot := true
 
 	rl.InitWindow(screenSize, screenSize, "Space Invaders")
 
 	backgroundImg := rl.LoadTexture("assets/space_background.png")
 	playerImg := rl.LoadTexture("assets/player.png")
+	enemyImg := rl.LoadTexture("assets/enemy.png")
+
+	enemyCount := 5
+	currentPosX := int32(75)
+	for enemyCount != 0 {
+		newEnemy := Enemy{
+			posX: currentPosX,
+			posY: 25,
+			img:  enemyImg,
+		}
+		enemies = append(enemies, newEnemy)
+		enemyCount--
+		currentPosX += 100
+	}
 
 	rl.SetTargetFPS(60)
 
@@ -32,6 +51,16 @@ func main() {
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawTexture(backgroundImg, 0, 0, rl.White)
 		rl.DrawTexture(playerImg, playerCoordinates[0], playerCoordinates[1], rl.White)
+
+		tempSliceE := enemies[:0]
+		for i, enemy := range enemies {
+			if enemy.posY <= 0 {
+				continue
+			}
+			rl.DrawTexture(enemy.img, enemy.posX, enemy.posY, rl.White)
+			tempSliceE = append(tempSliceE, enemies[i])
+		}
+		enemies = tempSliceE
 
 		tempSlice := bullets[:0]
 		for i, bullet := range bullets {
@@ -65,7 +94,6 @@ func main() {
 			}
 			bullets = append(bullets, newBullet)
 			shouldShoot = false
-			fmt.Println(bullets)
 		}
 
 		rl.EndDrawing()
