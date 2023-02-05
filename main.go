90bdbd946ec7f1c97314dfe0570dbd7fc3cefb81
gitSuki <collinbrown226@gmail.com>
2023-02-04 21:39:34 -0600

    Complete functionality for drawing the player and background

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
 	screenSize := int32(600)
+	playerCoordinates := [2]int32{25, 525}
+
 	rl.InitWindow(screenSize, screenSize, "Space Invaders")
 
-	backgroundTexture := rl.LoadTexture("assets/space_background.png")
+	backgroundImg := rl.LoadTexture("assets/space_background.png")
+	playerImg := rl.LoadTexture("assets/player.png")
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawTexture(backgroundTexture, 0, 0, rl.White)
+		rl.DrawTexture(backgroundImg, 0, 0, rl.White)
+		rl.DrawTexture(playerImg, playerCoordinates[0], playerCoordinates[1], rl.White)
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
