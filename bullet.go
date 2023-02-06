ae5008cbf9ca37e9b6ce007bef9799df8416b866
gitSuki <collinbrown226@gmail.com>
2023-02-06 12:15:40 -0600

    Deletes bullets on contact with enemy

--- bullet.go
+++ bullet.go
@@ -1,11 +1,13 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 type Bullet struct {
 	posX      int32
 	posY      int32
-	velocity  int32
+	speed     int32
 	width     int32
 	height    int32
 	collision rl.Rectangle
@@ -17,7 +19,7 @@ func appendBullet(b []Bullet, x int32, y int32) []Bullet {
 	newBullet := Bullet{
 		posX:      x + 22, // offset to make bullet origin at the player's center
 		posY:      y,
-		velocity:  10,
+		speed:     10,
 		width:     5,
 		height:    15,
 		collision: rl.NewRectangle(float32(x+22), float32(y), 5, 15),
@@ -32,15 +34,23 @@ func drawBullets(b []Bullet, e []Enemy) []Bullet {
 	// deleting the bullets that go off screen
 	var tempSlice []Bullet
 	for i := range b {
+		collidedWithEnemy := false
 		if b[i].posY <= 0 {
 			// deletes by not adding any bullets which have gone off screen to
 			// the temporary slice
 			continue
 		}
-		b[i].posY = b[i].posY - b[i].velocity
-		b[i].collision = rl.NewRectangle(float32(b[i].posX), float32(b[i].posY), float32(b[i].width), float32(b[i].height))
-		rl.DrawRectangle(b[i].posX, b[i].posY, b[i].width, b[i].height, b[i].color)
-		tempSlice = append(tempSlice, b[i])
+		for j := range e {
+			if rl.CheckCollisionRecs(b[i].collision, e[j].collision) {
+				collidedWithEnemy = true
+			}
+		}
+		if !collidedWithEnemy {
+			b[i].posY = b[i].posY - b[i].speed
+			b[i].collision = rl.NewRectangle(float32(b[i].posX), float32(b[i].posY), float32(b[i].width), float32(b[i].height))
+			rl.DrawRectangle(b[i].posX, b[i].posY, b[i].width, b[i].height, b[i].color)
+			tempSlice = append(tempSlice, b[i])
+		}
 	}
 	return tempSlice
 }
